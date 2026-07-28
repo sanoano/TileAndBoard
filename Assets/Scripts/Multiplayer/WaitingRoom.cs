0684af797b4602b37e3058d8ce6e5d9bcc8066ea
Endoran West <tomtomtomadwwest@gmail.com>
2026-07-28 20:07:02 +0100

    Add graphical interface to Ready-up system
    - Added new lobby UI
    - Added lobby timer
    - Also made a change that allowed the error messages for empty fields to fire. Before the game would try to load the code to try find a game if there were empty code or name fields.

--- Assets/Scripts/Multiplayer/WaitingRoom.cs
+++ Assets/Scripts/Multiplayer/WaitingRoom.cs
@@ -1,16 +1,21 @@
+using Mono.Cecil.Cil;
 using System;
 using System.Collections;
 using System.Threading.Tasks;
 using TMPro;
+using Tweens;
+using Unity.Entities.UniversalDelegates;
+using Unity.IO.LowLevel.Unsafe;
 using Unity.Netcode;
 using Unity.Services.Multiplayer;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using Tweens;
 
 public class WaitingRoom : NetworkBehaviour
 {
+    public static WaitingRoom Instance;
+
     private const string ReadyPropertyKey = "ready";
 
     private Lobby lobby;
@@ -18,38 +23,20 @@ public class WaitingRoom : NetworkBehaviour
     private bool isStartingGame;
 
     [Header("UI References")] 
-    [SerializeField] private TextMeshProUGUI sessionNameText;
-    [SerializeField] private TextMeshProUGUI joinCodeText;
-    [SerializeField] private TextMeshProUGUI playerListText;
-    [SerializeField] private GameObject waitingText;
-    private TextMeshProUGUI waitingStatusText;
-    [SerializeField] private GameObject startGameButtonObject;
-    private Button startGameButton;
     [SerializeField] private Button readyGameButton;
-    private TextMeshProUGUI readyGameButtonText;
-    [SerializeField] private Button leaveGameButton;
-    [SerializeField] private Image lensCap;
-    [SerializeField] private GameObject otherIsland;
-
-    [Header("Parameters")] 
-    [SerializeField] private float fadeDuration;
     
 
     private async void Start()
-    {
-        startGameButton = startGameButtonObject.GetComponent<Button>();
-        readyGameButtonText = readyGameButton.GetComponentInChildren<TextMeshProUGUI>();
-        waitingStatusText = waitingText.GetComponent<TextMeshProUGUI>();
-
+    {     
         lobby = NetworkManager.Singleton.gameObject.GetComponent<Lobby>();
-        startGameButton.onClick.AddListener(StartGame);
-        readyGameButton.onClick.AddListener(ToggleReady);
-        leaveGameButton.onClick.AddListener(LeaveGame);
+        
         lobby.m_NetworkManager.OnClientConnectedCallback += OnClientConnectedCallback;
         lobby.m_NetworkManager.OnClientDisconnectCallback += OnClientDisconnectCallback;
 
-        otherIsland.SetActive(false);
+        
         await SetReadyStateAsync(false);
+
+        UIManagerLobby.Instance.UpdateReadyButton(0);
     }
 
     private void Update()
@@ -57,45 +44,64 @@ public class WaitingRoom : NetworkBehaviour
         bool isSessionOwner = NetworkManager.Singleton.LocalClient.IsSessionOwner;
         bool canStartGame = CanStartGame();
 
-        startGameButtonObject.SetActive(isSessionOwner);
-        startGameButton.interactable = !isStartingGame && canStartGame;
-        waitingText.SetActive(!isSessionOwner);
-        otherIsland.SetActive(NetworkManager.Singleton.ConnectedClientsIds.Count == 2);
+        //startGameButtonObject.SetActive(isSessionOwner);
+        //startGameButton.interactable = !isStartingGame && canStartGame;
+        //waitingText.SetActive(!isSessionOwner);
 
-        UpdatePlayerList();
+        UIManagerLobby.Instance.EnableIsland(NetworkManager.Singleton.ConnectedClientsIds.Count == 2);
+
+        UpdatePlayerName();
+        UpdatePlayerStatus();
+        
         UpdateReadyButton();
         UpdateWaitingText();
-        
-        sessionNameText.text = lobby._session.Name;
-        joinCodeText.text = $"Join Code: {lobby._session.Code}";
+
+        UIManagerLobby.Instance.UpdateSessionInfo(lobby._session.Name, $"Join Code: {lobby._session.Code}");
+
+        if (canStartGame)
+        {
+            StartCoroutine(StartCountdown());
+        }
+            
+
     }
 
-    private void UpdatePlayerList()
+    private void UpdatePlayerName()
     {
-        playerListText.text = "";
-        
-        foreach (var player in lobby._session.Players)
+        for (int i = 0; i < lobby._session.Players.Count; i++)
         {
+            var player = lobby._session.Players[i];
+
             var name = player.GetPlayerName() ?? "Unknown";
             int suffixIndex = name.LastIndexOf('#');
             string trimmedName = suffixIndex > 0 ? name.Substring(0, suffixIndex) : name;
+
+            UIManagerLobby.Instance.UpdatePlayerName(i == 0, trimmedName);
+        }
+    }
+
+    private void UpdatePlayerStatus()
+    {
+        for (int i = 0; i < lobby._session.Players.Count; i++)
+        {
+            var player = lobby._session.Players[i];
             string readyStatus = IsPlayerReady(player) ? "READY" : "NOT READY";
 
-            playerListText.text += $"{trimmedName} [{readyStatus}]\n";
+            UIManagerLobby.Instance.UpdatePlayerStatus(i == 0, readyStatus);
         }
     }
 
     private void OnClientConnectedCallback(ulong id)
     {
-        UpdatePlayerList();
+        UpdatePlayerName();
     }
 
     private void OnClientDisconnectCallback(ulong id)
     {
-        UpdatePlayerList();
+        UpdatePlayerName();
     }
 
-    private async void ToggleReady()
+    public async void ToggleReady()
     {
         await SetReadyStateAsync(!IsLocalPlayerReady());
     }
@@ -134,23 +140,27 @@ public class WaitingRoom : NetworkBehaviour
 
     private void UpdateReadyButton()
     {
-        readyGameButton.interactable = !isStartingGame && !isUpdatingReadyState;
-        readyGameButtonText.text = IsLocalPlayerReady() ? "Unready" : "Ready Up";
+        if (!isStartingGame && !isUpdatingReadyState && !IsLocalPlayerReady())
+            UIManagerLobby.Instance.UpdateReadyButton(0);
+        else if (!isStartingGame && !isUpdatingReadyState && IsLocalPlayerReady())
+            UIManagerLobby.Instance.UpdateReadyButton(1);
+        else if (isStartingGame)
+            UIManagerLobby.Instance.UpdateReadyButton(2);
     }
 
     private void UpdateWaitingText()
     {
         if (NetworkManager.Singleton.ConnectedClientsIds.Count < 2)
         {
-            waitingStatusText.text = "Waiting for other player...";
+            UIManagerLobby.Instance.UpdateWaitingText(0);
         }
-        else if (AreAllPlayersReady())
+        else if (!AreAllPlayersReady())
         {
-            waitingStatusText.text = "Waiting for host to start...";
+            UIManagerLobby.Instance.UpdateWaitingText(1);
         }
         else
         {
-            waitingStatusText.text = "Waiting for both players to ready up...";
+            UIManagerLobby.Instance.UpdateWaitingText(2);
         }
     }
 
@@ -191,6 +201,36 @@ public class WaitingRoom : NetworkBehaviour
                isReady;
     }
 
+    public IEnumerator StartCountdown()
+    {//3 seconds are given after players are readied up and before starting the game
+
+        float timer = 0.0f;
+        float duration = 3.0f;
+
+        while (timer < duration)
+        {
+            if (!CanStartGame())
+            {
+                UIManagerLobby.Instance.CountdownUI("");
+                yield break;
+            }                
+
+            if (timer > 0 && timer < 1)
+                UIManagerLobby.Instance.CountdownUI("3");
+            else if (timer > 1 && timer < 2)
+                UIManagerLobby.Instance.CountdownUI("2");
+            else if (timer > 2  && timer < 3)
+                UIManagerLobby.Instance.CountdownUI("1");
+            else
+                UIManagerLobby.Instance.CountdownUI("0");
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        StartGame();   
+    }
+
     private void StartGame()
     {
         if (isStartingGame || !CanStartGame())
@@ -204,67 +244,24 @@ public class WaitingRoom : NetworkBehaviour
         {
             StartGameHandlerRpc(RpcTarget.Single(clientIds, RpcTargetUse.Temp));
         }
-
-        startGameButton.interactable = false;
     }
     
     [Rpc(SendTo.SpecifiedInParams)]
     private void StartGameHandlerRpc(RpcParams rpcParams = default)
     {
-        StartCoroutine(StartGameRoutine());
+        StartCoroutine(UIManagerLobby.Instance.StartGameRoutine());
     }
 
 
-    private IEnumerator StartGameRoutine()
-    {
-        
-        lensCap.gameObject.SetActive(true);
-        
-        Color opaque = new Color(0, 0, 0, 255);
-        
-        var backgroundTween = new ColorTween {
-            from = lensCap.color,
-            to = opaque,
-            duration = fadeDuration,
-            easeType = EaseType.ExpoInOut,
-            onUpdate = (_, value) => lensCap.color = value,
-        };
-
-        var instance = lensCap.gameObject.AddTween(backgroundTween);
-
-        yield return instance.AwaitDecommission();
-
-        if (NetworkManager.Singleton.LocalClient.IsSessionOwner)
-        {
-            NetworkManager.Singleton.SceneManager.LoadScene("BattleArena", LoadSceneMode.Single);
-        }
-        
-
-        yield return null;
-
-    }
-
-    private async void LeaveGame()
+    public async void LeaveGame()
     {
         await lobby.LeaveSessionAsync();
     }
 
+    
+
     public override void OnDestroy()
     {
-        if (startGameButton != null)
-        {
-            startGameButton.onClick.RemoveListener(StartGame);
-        }
-
-        if (readyGameButton != null)
-        {
-            readyGameButton.onClick.RemoveListener(ToggleReady);
-        }
-
-        if (leaveGameButton != null)
-        {
-            leaveGameButton.onClick.RemoveListener(LeaveGame);
-        }
 
         if (lobby?.m_NetworkManager != null)
         {
